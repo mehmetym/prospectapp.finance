799951497ede95da82ef10188c73d2fe37ada292
MEHMET YILDIRIM <97794083+mehmetym@users.noreply.github.com>
2026-08-05 01:47:32 +0300

    Update app.py

--- prospect-lab 2/app.py
+++ prospect-lab 2/app.py
@@ -8,7 +8,7 @@ Web: https://prospectapp.finance
 """
 import streamlit as st  
 import pandas as pd
-import numpy as np 
+import numpy as np  
 from datetime import datetime
 import yfinance as yf
 
